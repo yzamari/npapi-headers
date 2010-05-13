d777820e586eb0510adf8a11f42ebd33d2adf48b
stuart.morgan <stuart.morgan@9a2e1d2e-da24-3fc0-5ad9-6419310ec3da>
2010-05-13 21:54:56 +0000

    Update version 23 define for clarity

--- npapi.h
+++ npapi.h
@@ -746,7 +746,7 @@ enum NPEventType {
 #define NPVERS_HAS_ALL_NETWORK_STREAMS      20
 #define NPVERS_HAS_URL_AND_AUTH_INFO        21
 #define NPVERS_HAS_PRIVATE_MODE             22
-#define NPVERS_MACOSX_HAS_EVENT_MODELS      23
+#define NPVERS_MACOSX_HAS_COCOA_EVENTS      23
 
 /*----------------------------------------------------------------------*/
 /*                        Function Prototypes                           */
